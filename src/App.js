8fa3cf98860207ade154ca3aec853a7557acbf93
MarianaV22 <mar.veiga@ua.pt>
2025-06-01 15:11:21 +0100

    mudança de cookies para localstorage

--- src/App.js
+++ src/App.js
@@ -1,31 +1,110 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import React, { useEffect, useState } from 'react';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  useNavigate,
+} from 'react-router-dom';
 import AdminLayout from './layouts/AdminLayout';
 import Dashboard from './pages/Dashboard';
 import Users from './pages/Users';
 import Accessories from './pages/Accessories';
 import Tasks from './pages/Tasks';
 import PresetMessages from './pages/PresetMessages';
+import Login from './pages/Login';
+import { setAuthToken, setAuthUser, getAuthToken } from './utils/storageUtils';
+import api from './utils/api';
 
-import Login from './pages/Login'; 
+function AppRoutes() {
+  const navigate = useNavigate();
+  const [checkingAuth, setCheckingAuth] = useState(true);
 
-function App() {
-  return (
-    <Router>
-      <Routes>
-        {/* Rota para login */}
-        <Route path="/login" element={<Login />} />
+  useEffect(() => {
+
+    const params = new URLSearchParams(window.location.search);
+    const tokenFromUrl = params.get('token');
+
+    if (tokenFromUrl) {
+    
+      setAuthToken(tokenFromUrl);
+
+   
+      api
+        .get('/users/me')
+        .then((res) => {
+          setAuthUser(res.data);
+        
+          window.history.replaceState(
+            {},
+            document.title,
+            window.location.pathname
+          );
+          setCheckingAuth(false);
        
-        <Route element={<AdminLayout />}>
-          <Route path="/" element={<Dashboard />} />
-          <Route path="/users" element={<Users />} />
-          <Route path="/accessories" element={<Accessories />} />
-          <Route path="/tasks" element={<Tasks />} />
-           <Route path="/preset_messages" element={<PresetMessages />} />
-        </Route>
-      </Routes>
-    </Router>
+          navigate('/');
+        })
+        .catch((err) => {
+          console.error('Token inválido ou erro ao buscar usuário:', err);
+         
+          setAuthToken('');
+          window.history.replaceState(
+            {},
+            document.title,
+            window.location.pathname
+          );
+          setCheckingAuth(false);
+          navigate('/login');
+        });
+    } else {
+      
+      const token = getAuthToken();
+      if (!token) {
+        setCheckingAuth(false);
+        navigate('/login');
+      } else {
+      
+        api
+          .get('/users/me')
+          .then((res) => {
+            setAuthUser(res.data);
+            setCheckingAuth(false);
+         
+          })
+          .catch((err) => {
+            console.error('Token expirado ou inválido:', err);
+            setAuthToken('');
+            setCheckingAuth(false);
+            navigate('/login');
+          });
+      }
+    }
+
+  }, []);
+
+  if (checkingAuth) return null;
+
+  return (
+    <Routes>
+  
+      <Route path="/login" element={<Login />} />
+
+  
+      <Route element={<AdminLayout />}>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/users" element={<Users />} />
+        <Route path="/accessories" element={<Accessories />} />
+        <Route path="/tasks" element={<Tasks />} />
+        <Route path="/preset_messages" element={<PresetMessages />} />
+      </Route>
+    </Routes>
   );
 }
 
-export default App;
+export default function App() {
+  return (
+    <Router>
+      <AppRoutes />
+    </Router>
+  );
+}
--- src/utils/api.js
+++ src/utils/api.js
@@ -1,13 +1,14 @@
 
-import axios from 'axios';
-import { getCookie } from './cookieUtils';
+
+import axios from "axios";
+import { getAuthToken } from "./storageUtils";
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL
+  baseURL: process.env.REACT_APP_API_URL,
 });
 
-api.interceptors.request.use(config => {
-  const token = getCookie('token');
+api.interceptors.request.use((config) => {
+  const token = getAuthToken();
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
   }
